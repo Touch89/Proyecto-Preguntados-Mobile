02f67d2bdafcda4407fa5cd0a95cb687d28ef9af
Scorpion-CC <carloscantom@gmail.com>
2026-02-27 03:15:10 -0600

    Inicialización de variables
    Se crearon e inicializaron los componentes del juego para luego poder agregarles funcionalidad.

--- app/src/main/java/com/example/proyecto_preguntados_mobile/GameActivity.kt
+++ app/src/main/java/com/example/proyecto_preguntados_mobile/GameActivity.kt
@@ -1,12 +1,36 @@
 package com.example.proyecto_preguntados_mobile
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class GameActivity : AppCompatActivity() {
+
+    private lateinit var questionText: TextView
+
+    private lateinit var questionNumberText: TextView
+
+    private lateinit var totalAnsweredText: TextView
+
+    private lateinit var mainLayout: LinearLayout
+
+    private lateinit var optionAButton: Button
+    private lateinit var optionBButton: Button
+    private lateinit var optionCButton: Button
+    private lateinit var optionDButton: Button
+
+    private lateinit var prevButton: Button
+
+    private lateinit var nextButton: Button
+
+    private lateinit var hintButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +40,17 @@ class GameActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        questionText = findViewById(R.id.question_text)
+        questionNumberText = findViewById(R.id.questionNumber_text)
+        totalAnsweredText = findViewById(R.id.totalAnswered_text)
+        mainLayout = findViewById(R.id.main_layout)
+        optionAButton = findViewById(R.id.optionA_button)
+        optionBButton = findViewById(R.id.optionB_button)
+        optionCButton = findViewById(R.id.optionC_button)
+        optionDButton = findViewById(R.id.optionD_button)
+        prevButton = findViewById(R.id.prev_button)
+        nextButton = findViewById(R.id.next_button)
+        //hintButton = findViewById(R.id.hint_button) FALTA AGREGAR
     }
 }
