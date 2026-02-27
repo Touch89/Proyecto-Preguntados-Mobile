4adefa4090ced0e6c225e9adf7d36b81aabb0859
Scorpion-CC <carloscantom@gmail.com>
2026-02-27 17:13:00 -0600

    Avances en el desarrollo del juego.
    Se hicieron avances en la lógica del juego.

--- app/src/main/java/com/example/proyecto_preguntados_mobile/GameActivity.kt
+++ app/src/main/java/com/example/proyecto_preguntados_mobile/GameActivity.kt
@@ -1,13 +1,16 @@
 package com.example.proyecto_preguntados_mobile
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.getValue
 
 class GameActivity : AppCompatActivity() {
 
@@ -23,6 +26,23 @@ class GameActivity : AppCompatActivity() {
     private lateinit var nextButton: Button
     private lateinit var hintButton: Button
 
+    private val quizModel: QuizModel by viewModels()
+
+    private var questionArray = listOf<Question>()
+
+    private var questionIndex = 0
+    private var totalOfQuestions = 5
+
+    private var totalAnswered = 0
+    private var topicsChosen = listOf<String>()
+    private var counter = 0
+    //Agarrar los temas seleccionados
+
+    private fun updateInterface() {
+        questionNumberText.text = "Pregunta ${questionIndex + 1}"
+        totalAnsweredText.text = "${totalAnswered} / ${questionArray.size} contestadas"
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +53,6 @@ class GameActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         questionText = findViewById(R.id.question_text)
         questionNumberText = findViewById(R.id.questionNumber_text)
         totalAnsweredText = findViewById(R.id.totalAnswered_text)
@@ -45,5 +64,40 @@ class GameActivity : AppCompatActivity() {
         prevButton = findViewById(R.id.prev_button)
         nextButton = findViewById(R.id.next_button)
         //hintButton = findViewById(R.id.hint_button) FALTA AGREGAR
+
+        //prueba
+        topicsChosen += "Matemáticas"
+        topicsChosen += "Cultura General"
+
+        for (question in quizModel.questionList) {
+            if (counter < totalOfQuestions) {
+                if (topicsChosen.contains(question.topic)) {
+                    questionArray += question
+                }
+            }
+        }
+        questionText.text = questionArray[questionIndex].text
+        updateInterface()
+
+        //totalOfQuestions = variable pasada
+
+
+        nextButton.setOnClickListener { _ ->
+            questionIndex = (questionIndex + 1) % questionArray.size
+            questionText.text = questionArray[questionIndex].text
+            updateInterface()
+        }
+
+        prevButton.setOnClickListener { _ ->
+            if (questionIndex == 0) {
+                questionIndex = questionArray.size - 1
+            } else {
+                questionIndex = (questionIndex - 1) % questionArray.size
+            }
+            questionText.text = questionArray[questionIndex].text
+            updateInterface()
+        }
+
+
     }
 }
--- app/src/main/java/com/example/proyecto_preguntados_mobile/QuizModel.kt
+++ app/src/main/java/com/example/proyecto_preguntados_mobile/QuizModel.kt
@@ -1,6 +1,7 @@
 package com.example.proyecto_preguntados_mobile
 
 import androidx.lifecycle.ViewModel
+import kotlin.rem
 
 class QuizModel : ViewModel() {
     //Manera temporal para manejar las preguntas, puede que cambiemos la parte de las respuestas por una variable de la respuesta correcta y la lista de incorrectas***
@@ -29,8 +30,8 @@ class QuizModel : ViewModel() {
             "Ejemplo 3",
             "Cultura General",
             listOf<Answer>(
-                Answer("Respuesta 1", true),
-                Answer("Respuesta 2", false),
+                Answer("Respuesta 1", false),
+                Answer("Respuesta 2", true),
                 Answer("Respuesta 3", false),
                 Answer("Respuesta 4", false)
             )
@@ -39,22 +40,42 @@ class QuizModel : ViewModel() {
             "Ejemplo 4",
             "Otro tema",
             listOf<Answer>(
-                Answer("Respuesta 1", true),
+                Answer("Respuesta 1", false),
                 Answer("Respuesta 2", false),
                 Answer("Respuesta 3", false),
-                Answer("Respuesta 4", false)
+                Answer("Respuesta 4", true)
             )
         ),
         Question(
             "Ejemplo 5",
             "No sé",
             listOf<Answer>(
-                Answer("Respuesta 1", true),
+                Answer("Respuesta 1", false),
                 Answer("Respuesta 2", false),
-                Answer("Respuesta 3", false),
+                Answer("Respuesta 3", true),
                 Answer("Respuesta 4", false)
             )
         )
-
     )
+    private var questionIndex = 0
+    fun moveToTheNextQuestion() {
+        questionIndex = (questionIndex + 1) % questionArray.size
+    }
+
+    fun moveToThePrevQuestion() {
+        if (questionIndex == 0) {
+            questionIndex = questionArray.size - 1
+        } else {
+            questionIndex = (questionIndex - 1) % questionArray.size
+        }
+    }
+
+    val questionAnswer: List<Answer>
+        get() = questionArray[questionIndex].answers
+
+    val questionText: String
+        get() = questionArray[questionIndex].text
+
+    val questionList: List<Question>
+        get() = questionArray
 }
